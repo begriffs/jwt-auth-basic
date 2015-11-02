fc6dad116c0d809c608a1e5ee5d220c0b3f2adeb
Joe Nelson <cred+github@begriffs.com>
2015-11-01 20:25:09 -0800

    Beginnings of tokens route

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,40 @@ module Main where
 
 import Lib
 
-main :: IO ()
-main = undefined
+import Control.Monad (mzero)
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Maybe
+import Data.Monoid
+import Data.String.Conversions (cs)
+import Data.Text
+import Database.PostgreSQL.Simple
+import Network.HTTP.Types (status400, status403)
+import Web.Scotty
+import qualified Data.ByteString.Lazy as BL
+
+main = do
+  conn <- connectPostgreSQL "postgresql://j:@localhost:5432/temp"
+  scotty 3000 $ do
+    post "/tokens" $ do
+      payload <- body
+      case eitherDecode payload of
+        Right (LoginAttempt user pass) -> do
+          mRole <- liftIO $ loginRole conn user pass
+          case mRole of
+            Just role -> text $ cs role
+            Nothing ->
+              status status403
+        Left e -> do
+          status status400
+          text (cs e)
+
+data LoginAttempt = LoginAttempt {
+    laUser :: Text
+  , laPass :: Text
+  }
+instance FromJSON LoginAttempt where
+  parseJSON (Object o) =
+    LoginAttempt <$> o .: "user" <*> o .: "pass"
+  parseJSON _ = mzero
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,60 +1,12 @@
 module Lib where
 
+import Data.Text
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.SqlQQ
 
-authTableSql :: Query
-authTableSql = [sql|
-  create table auth (
-    user character varying not null,
-    pass character(60) not null,
-    role name not null,
-    constraint auth_pkey primary key (user)
-  );
-
-  create function check_role_exists() returns trigger
-    language plpgsql
-    as $$
-  begin
-    if not exists (select 1 from pg_roles as r where r.rolname = new.role) then
-      raise foreign_key_violation using message =
-        'unknown database role: ' || new.role;
-      return null;
-    end if;
-    return new;
-  end
-  $$;
-
-  create constraint trigger ensure_auth_role_exists
-    after insert or update on auth
-    for each row
-    execute procedure check_role_exists();
-
-  create function encrypt_pass() returns trigger
-    language plpgsql
-    as $$
-  begin
-    if tg_op = 'INSERT' or new.pass <> old.pass then
-      new.pass = crypt(new.pass, gen_salt('bf'));
-    end if;
-    return new;
-  end
-  $$;
-
-  create trigger protect_passwords
-    before insert or update on auth
-    for each row
-    execute procedure encrypt_pass(); |]
-
-loginRoleSql :: Query
-loginRoleSql = [sql|
-  create function
-  login_role(_login text, _pass text, out _matched_role text) returns text
-    language plpgsql
-    as $$
-  begin
-    select role into _matched_role from auth
-     where login = _login
-       and pass  = crypt(_pass, pass);
-  end;
-  $$; |]
+loginRole :: Connection -> Text -> Text -> IO (Maybe Text)
+loginRole c user pass = do
+  res <- query c "select login_role(?,?)" (user, pass)
+  return $ case res of
+    [Only (Just role)] -> Just role
+    _ -> Nothing
